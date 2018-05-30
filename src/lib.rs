2c8ceb747a20d3b53b7053a8d6a7152c33c8ea56
imvm <imvm@cin.ufpe.br>
2018-05-29 21:34:34 -0300

    updated crate versions

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,10 @@
-//#[macro_use]
-//extern crate vulkano;
+#[macro_use]
+extern crate vulkano;
+
+#[macro_use]
+extern crate vulkano_shader_derive;
+
+extern crate image;
 
 pub mod raytracer;
 
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,24 @@
+extern crate clap;
+use clap::{Arg, App};
+
 extern crate vko_tracer;
 
 fn main() {
+let matches = App::new("vko_tracer")
+                      .version("1.0")
+                      .author("Ian M. <imvm@cin.ufpe.br>")
+                      .about("Does raytracing")
+                      .arg(Arg::with_name("INPUT")
+                           .help("Sets the input file to use")
+                           .required(false)
+                           .index(1))
+                      .get_matches();
+
+    vko_tracer::raytracer::init();
+
+    vko_tracer::raytracer::process_file(matches.value_of("INPUT"));
+
     vko_tracer::raytracer::raytrace();
+
+    vko_tracer::raytracer::cleanup();
 }
